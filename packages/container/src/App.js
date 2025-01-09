a10cbbd972ef5b0aa4d3c6d1d3ab72ab46c62873
OFFICE\aghatkar <aghatkar@csod.com>
2025-01-09 13:18:24 +0530

    Initial Commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import MarketingApp from "./components/MarketingApp";
+export default () => {
+  return (
+    <div>
+      <div> Hello Container</div>
+      <hr></hr>
+      <MarketingApp></MarketingApp>
+    </div>
+  );
+};
